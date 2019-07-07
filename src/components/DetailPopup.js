12a8aadd922aa1d375f7bc2ebff7841f248fe0ea
EdgarJeremy <edgarpontoh3141@gmail.com>
2019-07-08 07:18:03 +0800

    (*) update step component. (+) additional information on detail popup regarding to the step progress

--- src/components/DetailPopup.js
+++ src/components/DetailPopup.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Table, Card, CardHeader, CardBody } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Table } from "reactstrap";
 import moment from "moment";
-import Steps, { Step } from 'rc-steps';
+import { List, Item } from 'react-step-component'
 
 import Loadable from "react-loading-overlay";
 import 'rc-steps/assets/index.css';
@@ -37,7 +37,7 @@ export default class DetailPopup extends React.Component {
       this.setState({ steps: data.rows });
     }).then(
       this.props.models.Track.collection.bind(this.props.models.Track, {
-        attributes: ['id', 'description', 'user_id', 'step_id'],
+        attributes: ['id', 'description', 'user_id', 'step_id', 'created_at', 'updated_at'],
         where: {
           registration_id: registration.id
         },
@@ -55,9 +55,56 @@ export default class DetailPopup extends React.Component {
   _findHandledBy(step_id) {
     const users = [];
     this.state.tracks.forEach((t, i) => {
-      if (t.step_id === step_id) users.push(t.user);
+      if (t.step_id === step_id) users.push({
+        user: t.user,
+        time: {
+          created_at: t.created_at,
+          updated_at: t.updated_at
+        }
+      });
     });
     return users;
+  }
+
+  _findRootTime(step_id) {
+    let time = {};
+    for (let i = 0; i < this.state.tracks.length; i++) {
+      const t = this.state.tracks[i];
+      if (t.step_id === step_id) {
+        time = {
+          created_at: t.created_at,
+          updated_at: t.updated_at
+        }
+        break;
+      }
+    }
+    return time;
+  }
+
+  _composeTime(time) {
+    if (!time) return {};
+    if (moment(time).isSame(new Date(), 'day')) {
+      return {
+        absolute: moment(time).format('HH:mm:ss'),
+        relative: moment(time).startOf('minute').fromNow().toUpperCase()
+      }
+    } else {
+      return {
+        absolute: moment(time).format('DD MMM YY').toUpperCase(),
+        relative: moment(time).startOf('minute').fromNow().toUpperCase(),
+      }
+    }
+  }
+
+  _getTotalTime(s, nU, nS) {
+    const tS = this._findRootTime(nS.id);
+    if (nU) {
+      const d = moment.duration(moment(moment(nU.time.created_at)).diff(s));
+      return d;
+    } else if (tS.created_at) {
+      const d = moment.duration(moment(moment(tS.created_at)).diff(s));
+      return d;
+    } else return;
   }
 
   render() {
@@ -65,63 +112,108 @@ export default class DetailPopup extends React.Component {
       <Modal isOpen={true} className="modal-primary modal-lg modal-wide">
         <ModalHeader toggle={this.props.toggle}>Detail Jejak Pendaftaran</ModalHeader>
         {this.state.ready ? (
-
-          <ModalBody className="lay">
-            <Card>
-              <CardHeader>
-                Informasi Pendaftaran
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead>
-                    <tr>
-                      <td><b>NAMA PEMOHON</b></td>
-                      <td>{this.props.registration.name}</td>
-                    </tr>
-                    <tr>
-                      <td><b>NIK PEMOHON</b></td>
-                      <td>{this.props.registration.nik}</td>
-                    </tr>
-                    {this.props.registration.purpose.form.map((f, i) => (
-                      <tr key={i}>
-                        <td><b>{f.name}</b></td>
-                        <td>{this.props.registration.data[f.name]}</td>
+          <div>
+            <ModalBody className="lay">
+              <div className="row">
+                <div className="col-md-5">
+                  <List>
+                    {
+                      this.state.steps.map((s, i) => (
+                        <div key={i}>
+                          <Item.Wrapper state={
+                            this.props.registration.step ?
+                              (
+                                s.step < this.props.registration.step.step ?
+                                  "done" : (
+                                    this.props.registration.step.step === s.step ?
+                                      "waiting" : null
+                                  )
+                              ) : "done"
+                          } end={i === this.state.steps.length - 1}>
+                            <Item.Left>
+                              <b className="track-step">{s.name.toUpperCase()}</b>
+                              <span className="track-sm">{s.description.toUpperCase()}</span>
+                            </Item.Left>
+                            <Item.Center content={(
+                              s.step < this.props.registration.step.step ?
+                                <i className="fa fa-check"></i> : <b>{s.step}</b>
+                            )} />
+                            <Item.Right>
+                              <b>{this._composeTime(this._findRootTime(s.id).created_at).absolute}</b>
+                              <span className="track-sm">{this._composeTime(this._findRootTime(s.id).created_at).relative}</span>
+                            </Item.Right>
+                          </Item.Wrapper>
+                          {
+                            this._findHandledBy(s.id).map((u, j) => (
+                              <Item.Wrapper key={j} active={this.props.registration.step.step === s.step && j === this._findHandledBy(s.id).length - 1} state={
+                                this.props.registration.step ?
+                                  (
+                                    s.step < this.props.registration.step.step ?
+                                      "done" : (
+                                        this.props.registration.step.step === s.step ?
+                                          "waiting" : null
+                                      )
+                                  ) : "done"
+                              } end={i === this.state.steps.length - 1}>
+                                <Item.Left>
+                                  <b className="track-user">{u.user.name.toUpperCase()}</b>
+                                  <span className="track-sm">({u.user.level.toUpperCase()} - {u.user.pending_user ? 'PENDING' : 'REGULAR'})</span>
+                                </Item.Left>
+                                <Item.Center size="sm" />
+                                <Item.Right>
+                                  <b className="track-time">{this._composeTime(u.time.created_at).absolute}</b>
+                                  <span className="track-sm">{this._composeTime(u.time.created_at).relative}</span>
+                                  <span className="track-sm">{(function () {
+                                    const d = this._getTotalTime(u.time.created_at, this._findHandledBy(s.id)[j + 1], this.state.steps[i + 1]);
+                                    if (d) {
+                                      if (d.days()) {
+                                        return `selama ${d.days()} hari ${d.hours()} jam ${d.minutes()} menit`;
+                                      } else if (d.hours()) {
+                                        return `selama ${d.hours()} jam ${d.minutes()} menit`;
+                                      } else if (d.minutes()) {
+                                        return `selama ${d.minutes()} menit`;
+                                      } else {
+                                        return `selama beberapa detik`;
+                                      }
+                                    } else return '-'
+                                  }).call(this)}</span>
+                                </Item.Right>
+                              </Item.Wrapper>
+                            ))
+                          }
+                        </div>
+                      ))
+                    }
+                  </List>
+                </div>
+                <div className="col-md-7">
+                  <Table responsive>
+                    <thead>
+                      <tr>
+                        <td><b>NAMA PEMOHON</b></td>
+                        <td>{this.props.registration.name}</td>
                       </tr>
-                    ))}
-                  </thead>
-                  <tbody>
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-
-            <Card>
-              <CardHeader>
-                Jejak Berkas
-  </CardHeader>
-              <CardBody>
-                <Steps icons={{ finish: <i className="fa fa-check"></i> }} current={this.props.registration.step ? this.props.registration.step.step - 1 : this.state.steps.length}>
-                  {this.state.steps.map((s, i) => (
-                    <Step key={i} title={s.name} description={
-                      <div className="step-desc">
-                        <b>Deskripsi : </b>
-                        <p>{s.description ? s.description : '--'}</p>
-                        <b>Pengurus : </b>
-                        <ol>
-                          {this._findHandledBy(s.id).map((u, i) => (
-                            <li key={i}>{u.name} ({u.pending_user ? 'PENDING' : 'REGULAR'})</li>
-                          ))}
-                        </ol>
-                      </div>
-                    } />
-                  ))}
-                </Steps>
-              </CardBody>
-            </Card>
+                      <tr>
+                        <td><b>NIK PEMOHON</b></td>
+                        <td>{this.props.registration.nik}</td>
+                      </tr>
+                      {this.props.registration.purpose.form.map((f, i) => (
+                        <tr key={i}>
+                          <td><b>{f.name}</b></td>
+                          <td>{this.props.registration.data[f.name]}</td>
+                        </tr>
+                      ))}
+                    </thead>
+                    <tbody>
+                    </tbody>
+                  </Table>
+                </div>
+              </div>
+            </ModalBody>
             <ModalFooter>
               <Button color="danger" onClick={this.props.onCancel} block>TUTUP</Button>
             </ModalFooter>
-          </ModalBody>
+          </div>
         ) : (
             <Loadable
               spinnerSize="100px"
